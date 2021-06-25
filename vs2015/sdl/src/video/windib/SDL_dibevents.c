7688f4996d30b3cf336b0fd8ff82a71b08130616
maron2000 <68574602+maron2000@users.noreply.github.com>
2021-06-25 22:13:12 +0900

    Amend JP keyboard related stuff.  - Added Virtual Keycode (windib/SDL_dibevents.c)  - Add comments for SDLkeycode (sdl/include/SDL_keysym.h)

--- vs2015/sdl/src/video/windib/SDL_dibevents.c
+++ vs2015/sdl/src/video/windib/SDL_dibevents.c
@@ -752,7 +752,13 @@ void DIB_InitOSKeymapPriv(void) {
 			VK_keymap[VK_OEM_4] = SDLK_LEFTBRACKET;
 			VK_keymap[VK_OEM_6] = SDLK_RIGHTBRACKET;
 			VK_keymap[VK_OEM_5] = SDLK_JP_YEN;
-			break;
+            //VK_keymap[VK_OEM_ENLW] = SDLK_WORLD_12; // hankaku toggle-1
+            //VK_keymap[VK_OEM_AUTO] = SDLK_WORLD_12; // hankaku toggle-2
+            //VK_keymap[VK_OEM_ATTN] = SDLK_CAPSLOCK;
+            //VK_keymap[VK_OEM_COPY] = SDLK_WORLD_15; // Hiragana
+            VK_keymap[VK_CONVERT] = SDLK_WORLD_14;  // Henkan
+            VK_keymap[VK_NONCONVERT] = SDLK_WORLD_13; // Muhenkan
+            break;
 	}
 
 	Arrows_keymap[3] = 0x25;
